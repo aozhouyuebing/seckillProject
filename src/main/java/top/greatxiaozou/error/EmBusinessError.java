81b790e212bc10ef1810ce7be5654d163134daef
aozhouyuebing <1065605174@qq.com>
2020-12-04 14:47:31 +0800

    开发第四天

---
+++ src/main/java/top/greatxiaozou/error/EmBusinessError.java
@@ -0,0 +1,39 @@
+package top.greatxiaozou.error;
+
+public enum EmBusinessError implements CommonError {
+    //通用错误类型10000
+    PARAMETER_VALIDATION_ERROR(10001,"参数不合法"),
+    UNKONW_ERROR(10002,"未知错误"),
+
+
+
+    //20000开头为用户信息相关错误定义
+    USER_NOT_EXIST(20001,"用户不存在"),
+    USER_LOFIN_FAIL(20002,"用户手机号或密码不正确"),
+    ;
+
+    private int errCode;
+    private String errMsg;
+
+
+    EmBusinessError(int errCode, String errMsg) {
+        this.errCode = errCode;
+        this.errMsg = errMsg;
+    }
+
+    @Override
+    public int getErrCode() {
+        return errCode;
+    }
+
+    @Override
+    public String getErrMsg() {
+        return errMsg;
+    }
+
+    @Override
+    public CommonError setErrMsg(String errMsg) {
+        this.errMsg = errMsg;
+        return this;
+    }
+}
---
+++ src/main/java/top/greatxiaozou/utils/EncodeUtils.java
@@ -0,0 +1,18 @@
+package top.greatxiaozou.utils;
+
+import sun.misc.BASE64Encoder;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class EncodeUtils {
+    public static String EncodeByMd5(String str) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        BASE64Encoder base64Encoder = new BASE64Encoder();
+
+        //加密字符串
+        String newStr = base64Encoder.encode(md5.digest(str.getBytes("utf-8")));
+        return newStr;
+    }
+}
